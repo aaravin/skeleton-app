a34b0f52f4e33741bac902ce64128a373d1aa332
Ashwin Aravindan <aaravin@ncsu.edu>
2016-10-28 10:05:21 -0400

    Fix webpack file loading path

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             },
             { 
                 test: /\.(ico|html|txt)$/,
-                loader: "file?name=[path][name].[ext]"
+                loader: "file?name=[name].[ext]"
             }
         ]
     }
